ad0f5a8a7aef50b6e5770a273b6729cfe69248b9
Akira Kageyama <sgks@mac.com>
2018-11-27 11:07:45 +0900

    burgers_equation/constantsn.f90: Change SI and DI

--- warming_up/burgers_equation/constants.f90
+++ warming_up/burgers_equation/constants.f90
@@ -16,10 +16,10 @@ module constants_m
   implicit none
 
   ! << f90 constants >>
-  integer, parameter :: SI = selected_int_kind(6)
-  integer, parameter :: DI = selected_int_kind(15)
+  integer, parameter :: SI = selected_int_kind(8)
+  integer, parameter :: DI = selected_int_kind(16)
   integer, parameter :: SR = selected_real_kind(6)
-  integer, parameter :: DR = selected_real_kind(15)
+  integer, parameter :: DR = selected_real_kind(12)
 
   ! << Mathematical constants >>
   real(DR), parameter :: PI = 3.1415926535897932_DR
