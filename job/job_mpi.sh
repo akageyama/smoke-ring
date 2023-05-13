326713158514d3ac0a50ca206935099b3f78d315
Akira Kageyama <sgks@mac.com>
2023-05-13 12:10:12 +0900

    edit

--- job/job_mpi.sh
+++ job/job_mpi.sh
@@ -4,12 +4,7 @@
 #      ./qsub job_mpi.sh
 #
 #PBS -q large
-#PBS -l select=2:ncpus=32:mpiprocs=32
-
-  ! 格子サイズのメモ
-  ! NX_GLOBAL = 302  ! Vis=2e-3, Kap=2e-3: 1000 steps 発散
-  ! NY_GLOBAL = 102  !
-  ! NZ_GLOBAL = 102  !
+#PBS -l select=4:ncpus=32:mpiprocs=32
 #PBS -l walltime=05:00:00
 #PBS -N job_h
 #PBS -o stdoe
@@ -18,4 +13,4 @@
 source /etc/profile.d/modules.sh
 module load compiler mpi
 cd ${PBS_O_WORKDIR}
-mpiexec -n 64 ../src/smoke_ring ../src/params.namelist
+mpiexec -n 128 ../src/smoke_ring ../src/params.namelist
